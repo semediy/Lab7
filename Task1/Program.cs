c761ddd83f87cd3e8f5654ad688d6aa5727c3ec2
semediy <c.semedii.yuliia@student.uzhnu.edu.ua>
2026-02-20 20:14:58 +0200

    Stage3

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<Action> actions = new List<Action>();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            actions.Add(() => Console.WriteLine(i));
+        }
+    }
+}
